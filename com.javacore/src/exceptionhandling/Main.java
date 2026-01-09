595af6395de4caf567fd583091a47201a9ca9b6f
garimajain <garima_jain@epam.com>
2026-01-09 10:17:44 +0530

    exception checked

--- com.javacore/src/exceptionhandling/Main.java
+++ com.javacore/src/exceptionhandling/Main.java
@@ -3,5 +3,6 @@ package exceptionhandling;
 public class Main {
     public static void main(String[] args) {
         System.out.println("exception");
+        System.out.println("garima checked exception");
     }
 }
