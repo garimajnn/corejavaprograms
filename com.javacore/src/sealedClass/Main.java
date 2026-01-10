1894289d915ccfc7f0e742024450fa6ff6c5555c
garimajain <garima_jain@epam.com>
2026-01-10 16:53:56 +0530

    sealed class

---
+++ com.javacore/src/sealedClass/Main.java
@@ -0,0 +1,34 @@
+package sealedClass;
+
+
+// sealed class must have a subclass
+//  class inheriting sealed class
+//       ---------> can be either sealed,non-sealed,final
+// permits should be the last keyword of the class after extends and implement
+
+
+sealed class A extends Object implements Cloneable permits B,C{
+
+}
+// here if I write final then none of the class can iherit B
+final class B extends A{
+
+}
+//here if C allows only then class can inherit it
+sealed class C extends A permits D{
+
+}
+
+// if a class is non-sealed then cny of the class can inherit it
+non-sealed class D extends C{
+
+}
+class E extends D{
+
+}
+
+
+
+public class Main {
+
+}
